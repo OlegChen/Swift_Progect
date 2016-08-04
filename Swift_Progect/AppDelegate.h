b8a184d7d5bd8b8ffb7cbb742a6cfcaf30f85cd2
OlegChen <chen@ACA803E2.ipt.aol.com>
2016-08-04 16:44:21 +0800

    初始项目

---
+++ Swift_Progect/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Swift_Progect
+//
+//  Created by Chen on 16/8/4.
+//  Copyright © 2016年 Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Swift_Progect/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Swift_Progect
+//
+//  Created by Chen on 16/8/4.
+//  Copyright © 2016年 Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
